a604e80a20423f7320b4d0d2af118b74a382a495
JMAC <maclellanj15@gmail.com>
2022-01-25 09:58:29 -0600

    added error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -17,7 +30,8 @@ var getRepoIssues = function(repo) {
             });
         } 
         else {
-            alert("There was a problem with your request!");
+            // if not successful, redirect to the homepage
+            document.location.replace("./index.html");
         }
     });
 };
@@ -70,4 +84,4 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+getRepoName();
